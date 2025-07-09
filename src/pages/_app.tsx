75bb0de8e9b4a1202e7fb0266de3b7ef5f99add0
joschonarth <joschonarth@gmail.com>
2025-07-09 19:57:03 -0300

    chore: setup ESLint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import { globalStyles } from "@/styles/global"
-import type { AppProps } from "next/app"
+import type { AppProps } from 'next/app'
+
+import { globalStyles } from '@/styles/global'
 
 globalStyles()
 
