929fd7345506de577f1c751925c17251d5b77d6e
joschonarth <joschonarth@gmail.com>
2025-07-09 20:06:46 -0300

    chore: setup Next page extensions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: [
+    'page.tsx',
+    'api.ts',
+    'api.tsx'
+  ]
 };
 
 export default nextConfig;
