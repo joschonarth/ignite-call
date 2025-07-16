87adaa1848ad72a5172851e1118c5c290a820efe
joschonarth <joschonarth@gmail.com>
2025-07-15 22:51:06 -0300

    feat: add Zod validation and transformation

--- src/pages/register/time-intervals/styles.ts
+++ src/pages/register/time-intervals/styles.ts
@@ -38,3 +38,8 @@ export const IntervalInputs = styled('div', {
     filter: 'invert(100%) brightness(40%)',
   },
 })
+
+export const FormError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$4',
+})
