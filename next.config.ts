c434c1300e6f018c114aa92c41378fd67f526219
joschonarth <joschonarth@gmail.com>
2025-07-23 20:07:11 -0300

    fix: allow ignoring ESLint during build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
