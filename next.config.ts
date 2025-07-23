1d3e86cc7252e32bd0d88934aeec627b175330e2
joschonarth <joschonarth@gmail.com>
2025-07-23 20:34:46 -0300

    fix: restore to dev environment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
 
 export default nextConfig
